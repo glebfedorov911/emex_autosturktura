a383ddbab1097df48faadc6b39968fdc9f72bdb5
gfedorov911 <fedorov22134@gmail.com>
2024-07-25 13:29:17 +0300

    remove secure=True

--- app/api_v1/users/views.py
+++ app/api_v1/users/views.py
@@ -38,7 +38,8 @@ async def auth_user(user_log: UserLogin, response: Response, session: AsyncSessi
     }
 
     token = encode_jwt(payload=payload)
-    response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite='Strict')
+    # response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite='Strict')
+    response.set_cookie(key="access_token", value=token, httponly=True, samesite='Strict')
 
     return TokenInfo(
         access_token=token,

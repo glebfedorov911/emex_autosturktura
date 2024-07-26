6c3fbf8f24050af75e6fddbbc139facb4defb87e
gfedorov911 <fedorov22134@gmail.com>
2024-07-27 01:31:23 +0300

    remove app. in dbhelper

--- app/core/models/db_helper.py
+++ app/core/models/db_helper.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_scop
 
 from asyncio import current_task
 
-from app.core.config import settings
+from core.config import settings
 
 
 class DataBaseHelper:

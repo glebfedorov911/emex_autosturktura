c8c95865e64603ff30b2e20e7d691b2daa43234b
gfedorov911 <fedorov22134@gmail.com>
2024-07-29 00:01:16 +0300

    add templates, new test.py file, add views.py

--- app/api_v1/path.py
+++ app/api_v1/path.py
@@ -1,6 +1,6 @@
 from app.api_v1.users.views import router as user_router
 from app.api_v1.proxies.views import router as proxy_router
-from app.api_v1.parser.views import router as websocket_router
+from app.api_v1.parser.test import router as websocket_router
 from app.core.config import settings
 
 from fastapi import APIRouter

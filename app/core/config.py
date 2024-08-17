696d438a2a1491679f58688e9cd5dbdae93602b4
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2024-08-17 16:08:52 +0300

    initial

--- app/core/config.py
+++ app/core/config.py
@@ -13,8 +13,8 @@ BASE_DIR =  Path(__file__).parent.parent
 DB_PATH = BASE_DIR / "db.sqlite3"
 
 class DBSettings(BaseModel):
-    # url: str = f"sqlite+aiosqlite:///{DB_PATH}"
-    url: str = f"{os.getenv("DATABASE_URL")}"
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    #url: str = f"{os.getenv("DATABASE_URL")}"
     echo: bool = False
 
 class Proxy(BaseModel):
--- main.py
+++ main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 app.include_router(router=router)
 
 origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1",
+    "https://localhost",
+    "https://localhost:8000",
+    "https://127.0.0.1:8000",
+    "https://127.0.0.1","http://localhost:5173"
 ]
 
 app.add_middleware(

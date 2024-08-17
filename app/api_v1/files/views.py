c67d93063981b1d6c2854e14721345300522be69
gfedorov911 <fedorov22134@gmail.com>
2024-08-17 14:07:56 +0300

    edit commas

--- app/api_v1/files/views.py
+++ app/api_v1/files/views.py
@@ -17,7 +17,7 @@ router = APIRouter(prefix="/files", tags=["Files"])
 @router.post("/upload_file/")
 async def upload_file(file: UploadFile = File(...), session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
     directory = settings.upload.path_for_upload
-    unique_filename = get_unique_filename(directory, f"{payload.get("username")}_дляпарсинг.{file.filename.split('.')[-1]}")
+    unique_filename = get_unique_filename(directory, f"{payload.get('username')}_дляпарсинг.{file.filename.split('.')[-1]}")
     # unique_filename = get_unique_filename(directory, f"{payload.get("username")}_дляпарсинг.xlsx")
     file_location = os.path.join(directory, unique_filename)
 

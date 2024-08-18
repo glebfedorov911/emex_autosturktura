5e554ea8f1890aaeb09a7b68f7b27ce4a4283e1c
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2024-08-18 18:06:38 +0300

    cookie fixes

--- app/api_v1/users/views.py
+++ app/api_v1/users/views.py
@@ -8,24 +8,35 @@ from . import crud
 from .crud import get_payload
 from .token_info import TokenInfo
 from app.api_v1.auth.utils import encode_jwt
-from app.core.models import User    
-from .depends import exception_admin    
+from app.core.models import User
+from .depends import exception_admin
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
+
 @router.post("/sign_up/")
-async def create_user(user_in: UserCreate, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def create_user(
+    user_in: UserCreate,
+    session: AsyncSession = Depends(db_helper.session_depends),
+    payload=Depends(get_payload),
+):
     exception_admin(payload)
 
     return await crud.create_user(user_in=user_in, session=session)
 
+
 @router.post("/login/", response_model=TokenInfo)
-async def auth_user(user_log: UserLogin, response: Response, session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Cookie(default=None)):
+async def auth_user(
+    user_log: UserLogin,
+    response: Response,
+    session: AsyncSession = Depends(db_helper.session_depends),
+    access_token: str | None = Cookie(default=None),
+):
     if access_token:
         raise HTTPException(
             status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-            detail="Вы уже авторизованы | You have already auth"
+            detail="Вы уже авторизованы | You have already auth",
         )
     user = await crud.validate_user(user_log=user_log, session=session)
 
@@ -34,54 +45,80 @@ async def auth_user(user_log: UserLogin, response: Response, session: AsyncSessi
         "username": user.username,
         "description": user.description,
         "fullname": user.fullname,
-        "is_admin": user.is_admin
+        "is_admin": user.is_admin,
     }
 
     token = encode_jwt(payload=payload)
     # response.set_cookie(key="access_token", value=token, httponly=True, secure=True, samesite='Strict')
-    response.set_cookie(key="access_token", value=token, httponly=False, samesite='None',secure=True)
-
-    return TokenInfo(
-        access_token=token,
-        token_type="Bearer"
+    response.set_cookie(
+        key="access_token",
+        value=token,
+        httponly=False,
+        samesite="None",
+        secure=True,
+        max_age=3600,
     )
 
+    return TokenInfo(access_token=token, token_type="Bearer")
+
+
 @router.get("/me/")
-async def get_me(payload = Depends(get_payload)):
+async def get_me(payload=Depends(get_payload)):
     return payload
+
 
 @router.get("/logout/")
 async def logout(response: Response, access_token: str | None = Cookie(default=None)):
     if not access_token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Вы не авторизованы | You are not auth"
+            detail="Вы не авторизованы | You are not auth",
         )
-    response.delete_cookie(key="access_token")
-    return {
-        "msg": "Вы успешно вышли | Success logout"
-    }
+    response.delete_cookie(
+        key="access_token", httponly=False, secure=True, samesite="none"
+    )
+    return {"msg": "Вы успешно вышли | Success logout"}
+
 
 @router.get("/show_all/")
-async def show_all_users(session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def show_all_users(
+    session: AsyncSession = Depends(db_helper.session_depends),
+    payload=Depends(get_payload),
+):
     exception_admin(payload=payload)
-    
+
     return await crud.show_all_users(session=session)
 
+
 @router.get("/about_one/{user_id}/")
-async def about_one_user(user_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def about_one_user(
+    user_id: int,
+    session: AsyncSession = Depends(db_helper.session_depends),
+    payload=Depends(get_payload),
+):
     exception_admin(payload=payload)
 
     return await crud.about_one_user(user_id=user_id, session=session)
 
+
 @router.patch("/edit/{user_id}")
-async def edit_user(user_id: int, upd_user: UserUpdate, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def edit_user(
+    user_id: int,
+    upd_user: UserUpdate,
+    session: AsyncSession = Depends(db_helper.session_depends),
+    payload=Depends(get_payload),
+):
     exception_admin(payload=payload)
 
     return await crud.edit_user(user_id, upd_user, session)
 
+
 @router.delete("/delete/{user_id}")
-async def delete_user(user_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def delete_user(
+    user_id: int,
+    session: AsyncSession = Depends(db_helper.session_depends),
+    payload=Depends(get_payload),
+):
     exception_admin(payload=payload)
 
     return await crud.delete_user(user_id, session)
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ origins = [
     "https://localhost",
     "https://localhost:8000",
     "https://127.0.0.1:8000",
-    "https://127.0.0.1","http://localhost:5173"
+    "https://127.0.0.1","http://localhost:5173", "http://127.0.0.1:5173"
 ]
 
 app.add_middleware(

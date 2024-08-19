32d7b000953511de0533330af8c19e1b160af308
glebfedorov911 <fedorov22134@gmail.com>
2024-08-20 01:29:53 +0300

    edit test2.html file (delete status), create parser without saving, edit saving schemas for proxy, add all functional for parsing(witout saving)

--- app/api_v1/proxies/schemas.py
+++ app/api_v1/proxies/schemas.py
@@ -4,9 +4,10 @@ from datetime import datetime
 
 
 class ProxySchemas(BaseModel):
-    id_proxy: int
+    id_proxy: str
     login: str
     password: str
     ip_with_port: str
     expired_at: datetime
     user_id: int
+    _is_banned: bool = False

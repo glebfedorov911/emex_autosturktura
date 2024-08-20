d37798e733fc763150fbf3c8add4ca0ddeacaf54
gfedorov911 <fedorov22134@gmail.com>
2024-08-20 23:59:59 +0300

    delete bad parser

--- app/api_v1/path.py
+++ app/api_v1/path.py
@@ -2,7 +2,6 @@ from app.api_v1.users.views import router as user_router
 from app.api_v1.proxies.views import router as proxy_router
 from app.api_v1.filters.views import router as filter_router
 from app.api_v1.files.views import router as file_router
-from app.api_v1.parser.views import router as parser_router
 from app.api_v1.showing.views import router as show_data_router
 from app.api_v1.parser_2.views import router as parser2_rounter
 from app.core.config import settings
@@ -15,6 +14,5 @@ router.include_router(user_router)
 router.include_router(proxy_router)
 router.include_router(filter_router)
 router.include_router(file_router)
-router.include_router(parser_router)
 router.include_router(show_data_router)
 router.include_router(parser2_rounter)

04fe9b8aac586ee19ac38c8471d9c9d9ed3d86c2
gfedorov911 <fedorov22134@gmail.com>
2024-08-24 00:55:39 +0300

    add to database when start

---
+++ app/core/add_database.py
@@ -0,0 +1,171 @@
+import psycopg2
+import os
+
+from datetime import datetime
+from psycopg2 import sql
+from dotenv import load_dotenv
+
+from app.api_v1.auth.utils import hash_password
+
+
+load_dotenv()
+
+tables = ["files", "parsers", "proxys"]
+
+db_config = {
+    'dbname': os.getenv("DBNAME"),
+    'user': os.getenv("USER"),
+    'password': os.getenv("PASSWORD"),
+    'host': os.getenv("HOST"),
+    'port': os.getenv("PORT"),      
+}
+
+def execute_query(conn, query, params=None):
+    with conn.cursor() as cursor:
+        cursor.execute(query, params)
+        conn.commit()
+
+conn = psycopg2.connect(**db_config)
+
+def add_to_db(table, data_add):
+    check_query = sql.SQL(f"SELECT COUNT(*) FROM {table}")
+    with conn.cursor() as cursor:
+        cursor.execute(check_query)
+        count = cursor.fetchone()[0]
+
+    if count != 0:
+        return
+
+    for values in data_add:
+        val = ', '.join(["%s"]*len(values.values()))
+        insert_query = sql.SQL(f"""
+            INSERT INTO {table} ({', '.join(list(values.keys()))})
+            VALUES ({val})
+        """)
+
+        execute_query(conn, insert_query, list(values.values()))
+
+data_add = {
+    "users": (
+        {"fullname": "string", "description": "string", "username": "string", "password": bytes(hash_password(os.getenv("string_PASSWORD"))), "is_admin": "True", "is_parsing": "False"},
+        {"fullname": "string_admin", "description": "string_admin", "username": "string_admin", "password": bytes(hash_password(os.getenv("string_admin_PASSWORD"))), "is_admin": "True", "is_parsing": "False"},
+        {"fullname": "admin_fullname", "description": "admin_description", "username": "admin", "password": bytes(hash_password(os.getenv("ADMIN_PASSWORD"))), "is_admin": "True", "is_parsing": "False"},
+        {"fullname": "newuser_fullname", "description": "newuser_description", "username": "newuser", "password": bytes(hash_password(os.getenv("NEWUSER_PASSWORD"))), "is_admin": "False", "is_parsing": "False"},
+        {"fullname": "simpleuser_fullname", "description": "simpleuser_description", "username": "simpleuser", "password": bytes(hash_password(os.getenv("SIMPLE_PASSWORD"))), "is_admin": "False", "is_parsing": "False"},
+    ),
+}
+
+add_to_db("users", data_add["users"])
+
+check_query1 = sql.SQL("SELECT id FROM users WHERE username = 'admin'")
+check_query2 = sql.SQL("SELECT id FROM users WHERE username = 'newuser'")
+check_query3 = sql.SQL("SELECT id FROM users WHERE username = 'simpleuser'")
+with conn.cursor() as cursor:
+    cursor.execute(check_query1)
+    id_admin_fullname = cursor.fetchone()[0]
+    cursor.execute(check_query2)
+    id_newuser_fullname = cursor.fetchone()[0]
+    cursor.execute(check_query3)
+    id_simpleuser_fullname = cursor.fetchone()[0]
+
+data_add = {
+    "newfilters": (
+        {"deep_filter": "10", "deep_analog": "10", "analog": "False", "is_bigger": "True", "date": "5", "logo": "HXAW", "user_id": id_admin_fullname, "title": "string1"},
+        {"deep_filter": "50", "deep_analog": "25", "analog": "False", "is_bigger": "False", "date": "25", "logo": "MARO", "user_id": id_newuser_fullname, "title": "string2"},
+        {"deep_filter": "15", "deep_analog": "25", "analog": "True", "is_bigger": "False", "date": "25", "logo": "RXGF", "user_id": id_simpleuser_fullname, "title": "string3"},
+        {"deep_filter": "10", "deep_analog": "25", "analog": "False", "is_bigger": "False", "date": "25", "logo": "RXGF", "user_id": id_simpleuser_fullname, "title": "string4"},
+        {"deep_filter": "12", "deep_analog": "1", "analog": "False", "logo": "RXGF", "user_id": id_simpleuser_fullname, "title": "string5"},
+        {"deep_filter": "33", "deep_analog": "31", "analog": "False", "logo": "RXGF", "user_id": id_simpleuser_fullname, "title": "string6"},
+        {"deep_filter": "11", "deep_analog": "23", "analog": "False", "is_bigger": "True", "date": "5", "logo": "OEOL", "user_id": id_newuser_fullname, "title": "string7"},
+        {"deep_filter": "11", "deep_analog": "10", "analog": "False", "is_bigger": "False", "date": "10", "logo": "OEOL", "user_id": id_newuser_fullname, "title": "string8"},
+        {"deep_filter": "11", "deep_analog": "10", "analog": "False", "is_bigger": "False", "date": "10", "logo": "RMFZ", "user_id": id_admin_fullname, "title": "string9"},
+        {"deep_filter": "11", "deep_analog": "10", "analog": "False", "is_bigger": "False", "date": "10", "logo": "RMFZ", "user_id": id_admin_fullname, "title": "string10"},
+    ),
+}
+
+add_to_db("newfilters", data_add["newfilters"])
+
+check_query4 = sql.SQL("SELECT id FROM newfilters WHERE title = 'string2'")
+check_query5 = sql.SQL("SELECT id FROM newfilters WHERE title = 'string9'")
+with conn.cursor() as cursor:
+    cursor.execute(check_query4)
+    id_string2_filter = cursor.fetchone()[0]
+    cursor.execute(check_query5)
+    id_string9_filter = cursor.fetchone()[0]
+    
+data_add = {
+    "files": (
+        {"before_parsing_filename": "test1_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_admin_fullname},
+        {"before_parsing_filename": "test1_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_newuser_fullname},
+        {"before_parsing_filename": "test1_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_simpleuser_fullname},
+        {"before_parsing_filename": "test2_дляпарсинг.xlsx", "after_parsing_filename": "test2_послепарсинга.xlsx", "date": str(datetime.now()), "user_id": id_newuser_fullname, "new_filter_id": id_string2_filter},
+        {"before_parsing_filename": "test2_дляпарсинг.xlsx", "after_parsing_filename": "test2_послепарсинга.xlsx", "date": str(datetime.now()), "user_id": id_newuser_fullname, "new_filter_id": id_string2_filter},
+        {"before_parsing_filename": "test2_дляпарсинг.xlsx", "after_parsing_filename": "test2_послепарсинга.xlsx", "date": str(datetime.now()), "user_id": id_admin_fullname, "new_filter_id": id_string9_filter},
+        {"before_parsing_filename": "test3_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_admin_fullname},
+        {"before_parsing_filename": "test3_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_newuser_fullname},
+        {"before_parsing_filename": "test3_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_admin_fullname},
+        {"before_parsing_filename": "test3_дляпарсинг.xlsx", "after_parsing_filename": "None", "date": str(datetime.now()), "user_id": id_simpleuser_fullname},
+    ),
+    "parsers": (
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_admin_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_admin_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_admin_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_admin_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_admin_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_newuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_newuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_newuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_newuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_newuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_simpleuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_simpleuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_simpleuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "Peugeot---Citroen", "number_of_goods": "362312", "logo": "SRIH", "delivery": "8", "best_price": "220", "user_id": id_simpleuser_fullname, "quantity_goods": "15", "price_with_logo": "490"},
+        {"article": "Mahle---Knecht", "number_of_goods": "02943N0", "logo": "VKVV", "delivery": "114", "best_price": "154778", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+        {"article": "ГАЗ", "number_of_goods": "6270000290", "logo": "RUZB", "delivery": "8", "best_price": "2896", "user_id": id_simpleuser_fullname, "quantity_goods": "1", "price_with_logo": "Нет такого лого среди оригиналов"},
+    ),
+    "proxys": (
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6460790", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_admin_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491400", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.97.212:1050", "_is_banned": "False", "user_id": id_admin_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491399", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_admin_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481859", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://109.248.166.189:1050", "_is_banned": "False", "user_id": id_admin_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481858", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://91.188.244.80:1050", "_is_banned": "False", "user_id": id_admin_fullname, "is_using": "True"},
+
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6460790", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_newuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491400", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.97.212:1050", "_is_banned": "False", "user_id": id_newuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491399", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_newuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481859", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://109.248.166.189:1050", "_is_banned": "False", "user_id": id_newuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481858", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://91.188.244.80:1050", "_is_banned": "False", "user_id": id_newuser_fullname, "is_using": "True"},
+
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6460790", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_simpleuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491400", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.97.212:1050", "_is_banned": "False", "user_id": id_simpleuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6491399", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://194.156.123.115:1050", "_is_banned": "False", "user_id": id_simpleuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481859", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://109.248.166.189:1050", "_is_banned": "False", "user_id": id_simpleuser_fullname, "is_using": "True"},
+        {"expired_at": "2024-08-26 00:00:00", "id_proxy": "6481858", "login": os.getenv("login_proxy"), "password": os.getenv("password_proxy"), "ip_with_port": "http://91.188.244.80:1050", "_is_banned": "False", "user_id": id_simpleuser_fullname, "is_using": "True"},
+    ),
+}
+
+for table in tables:
+    add_to_db(table, data_add[table])

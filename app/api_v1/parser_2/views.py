8af7678592dc40e16df783112e7f420bf37a747a
gfedorov911 <fedorov22134@gmail.com>
2024-08-23 16:12:26 +0300

    edit result file

--- app/api_v1/parser_2/views.py
+++ app/api_v1/parser_2/views.py
@@ -112,9 +112,8 @@ async def websocket_status_endpoint(
     if files is None:
         user_data[payload.get("sub")]["status"] = "Файл спаршен либо не загружен"
     else:
-        result_file_name = (
-            payload.get("username") + "_послепарсинга." + files.split(".")[-1]
-        )
+        result_file_name = get_unique_filename(str(settings.upload.path_for_upload), f"{payload.get('username')}_послепарсинга.xlsx")
+        print(result_file_name)
     await websocket.accept()
     try:
         while True:

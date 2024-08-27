6c9208d1dae3f4b1d44d8a120ddb3e0dfef4d8ba
gfedorov911 <fedorov22134@gmail.com>
2024-08-28 01:12:30 +0300

    edit views in filters

--- app/api_v1/filters/views.py
+++ app/api_v1/filters/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,26 +6,37 @@ from .schemas import FilterCreate, FilterUpdate
 from app.api_v1.users.crud import get_payload
 from app.core.models import db_helper
 from app.api_v1.filters import crud
+from app.api_v1.auth.depends import check_payload
 
 
 router = APIRouter(prefix="/filters", tags=["Filters"])
 
 @router.post("/create_filter")
-async def create_filter(filter_in: FilterCreate, session: AsyncSession = Depends(db_helper.session_depends), payload=Depends(get_payload)):
+async def create_filter(filter_in: FilterCreate, session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Header(default=None, convert_underscores=False)):
+    payload = await check_payload(access_token=access_token)
+
     return await crud.create_filter(session=session, filter_in=filter_in, user_id=payload.get("sub"))
 
 @router.get("/get_filters")
-async def get_filter(session: AsyncSession = Depends(db_helper.session_depends), payload=Depends(get_payload)):
+async def get_filter(session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Header(default=None, convert_underscores=False)):
+    payload = await check_payload(access_token=access_token)
+
     return await crud.get_filter(session=session, user_id=payload.get("sub"))
 
 @router.get("/get_filter/{filter_id}")
-async def get_filter_by_id(filter_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload=Depends(get_payload)):
+async def get_filter_by_id(filter_id: int, session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Header(default=None, convert_underscores=False)):
+    payload = await check_payload(access_token=access_token)
+
     return await crud.get_filter_by_id(session=session, user_id=payload.get("sub"), filter_id = filter_id)
 
 @router.patch("/edit_filter/{filter_id}")
-async def edit_filter(filter_id: int, upd_filter: FilterUpdate, session: AsyncSession = Depends(db_helper.session_depends), payload=Depends(get_payload)):
+async def edit_filter(filter_id: int, upd_filter: FilterUpdate, session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Header(default=None, convert_underscores=False)):
+    payload = await check_payload(access_token=access_token)
+
     return await crud.edit_filter(filter_id=filter_id, upd_filter=upd_filter, user_id=payload.get("sub"), session=session)
 
 @router.delete("/delete_filter/{filter_id}")
-async def delete_router(filter_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload=Depends(get_payload)):
+async def delete_router(filter_id: int, session: AsyncSession = Depends(db_helper.session_depends), access_token: str | None = Header(default=None, convert_underscores=False)):
+    payload = await check_payload(access_token=access_token)
+
     return await crud.delete_filter(session=session, user_id=payload.get("sub"), filter_id=filter_id)

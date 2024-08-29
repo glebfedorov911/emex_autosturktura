094204aa2ce5ce9faf73fb45063066b9b321849d
gfedorov911 <fedorov22134@gmail.com>
2024-08-29 23:46:21 +0300

    edit views in files

--- app/api_v1/files/views.py
+++ app/api_v1/files/views.py
@@ -45,7 +45,7 @@ async def upload_file(file: UploadFile = File(...), session: AsyncSession = Depe
         "message": f"Файл {unique_filename} сохранен по пути {file_location}"
     }
 
-@router.post("/download_file")
+@router.get("/download_file")
 async def download_last_file(session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
     
 
@@ -55,11 +55,11 @@ async def download_last_file(session: AsyncSession = Depends(db_helper.session_d
     file_location = os.path.join(settings.upload.path_for_upload, last_file.after_parsing_filename)
     return FileResponse(path=file_location, filename=last_file.after_parsing_filename)
 
-@router.post("/download_file/before_parsing/{file_id}")
-async def download_file(file_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+@router.get("/download_file/before_parsing/{file_id}")
+async def download_file(file_id: int, session: AsyncSession = Depends(db_helper.session_depends)):
     
 
-    filename = await crud.get_files_by_id(session=session, user_id=payload.get("sub"), file_id=file_id)
+    filename = await crud.get_files_by_id(session=session, file_id=file_id)
     file_location = os.path.join(settings.upload.path_for_upload, filename.before_parsing_filename)
     if not os.path.exists(file_location):
         raise HTTPException(
@@ -68,11 +68,11 @@ async def download_file(file_id: int, session: AsyncSession = Depends(db_helper.
         )
     return FileResponse(path=file_location, filename=filename.before_parsing_filename)
 
-@router.post("/download_file/after_parsing/{file_id}")
-async def download_file(file_id: int, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+@router.get("/download_file/after_parsing/{file_id}")
+async def download_file(file_id: int, session: AsyncSession = Depends(db_helper.session_depends)):
     
 
-    filename = await crud.get_files_by_id(session=session, user_id=payload.get("sub"), file_id=file_id)
+    filename = await crud.get_files_by_id(session=session, file_id=file_id)
     check_has_last_file_after_parsing(filename)
     file_location = os.path.join(settings.upload.path_for_upload, filename.after_parsing_filename)
     if not os.path.exists(file_location):

c2aacd9bee5270c38fc7fbf2d9b44d07a3a0514d
gfedorov911 <fedorov22134@gmail.com>
2024-08-29 23:46:40 +0300

    edit views in showing

--- app/api_v1/showing/views.py
+++ app/api_v1/showing/views.py
@@ -11,6 +11,6 @@ from . import crud
 router = APIRouter(prefix="/showing", tags=["Show"])
 
 @router.get("/show_data")
-async def show_data(skip: int = 0, limit: int = 10, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
+async def show_data(file_id: int, skip: int = 0, limit: int = 10, session: AsyncSession = Depends(db_helper.session_depends), payload = Depends(get_payload)):
     
-    return await crud.show_data(session=session, user_id=payload.get("sub"), skip=skip, limit=limit)
+    return await crud.show_data(session=session, user_id=payload.get("sub"), skip=skip, limit=limit, file_id=file_id)

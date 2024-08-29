7ff6326a964d59a9c18e6eded8cd0510d469891d
gfedorov911 <fedorov22134@gmail.com>
2024-08-29 23:49:24 +0300

    edit crud in showing

--- app/api_v1/showing/crud.py
+++ app/api_v1/showing/crud.py
@@ -5,8 +5,8 @@ from sqlalchemy import select
 from app.core.models import Parser
 
 
-async def show_data(session: AsyncSession, user_id: int, skip: int, limit: int):
-    stmt = select(Parser).where(Parser.user_id==user_id).offset(skip).limit(limit)
+async def show_data(session: AsyncSession, user_id: int, skip: int, limit: int, file_id: int):
+    stmt = select(Parser).where(Parser.user_id==user_id).where(Parser.file_id==file_id).offset(skip).limit(limit)
     result: Result = await session.execute(stmt)
     data = result.scalars().all()
 

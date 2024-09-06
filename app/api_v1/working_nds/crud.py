d9044ce1ce9826656a142fdaeac4028865453840
gfedorov911 <fedorov22134@gmail.com>
2024-09-06 19:10:13 +0300

    add nds endpoint

---
+++ app/api_v1/working_nds/crud.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.engine import Result
+from sqlalchemy import select
+
+from app.core.models import Parser
+from .schemas import ParserCreate
+from .depends import *
+
+
+async def edit_with_nds(session: AsyncSession, user_id: int, file_id: int):
+    parsers_data = await get_all_data_from_file(session=session, user_id=user_id, file_id=file_id)
+
+    for data in parsers_data:
+        best_price = edit_price(data)
+        if not best_price:
+            continue
+        
+        data.best_price = str(best_price)
+        session.add(data)
+        await session.commit()
+    
+    filename = await get_filename(file_id=file_id, user_id=user_id, session=session)
+    await to_file(filename=filename, parser_data=await get_all_data_from_file(session=session, user_id=user_id, file_id=file_id))
+    await set_filename(file_id=file_id, session=session, user_id=user_id)
+
+    return parsers_data

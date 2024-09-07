b197da2df87c541e43eea00ff045faf7ed08d351
VYACHESLAV204 <125289802+VYACHESLAV204@users.noreply.github.com>
2024-09-07 13:36:42 +0300

    change orirgins

--- main.py
+++ main.py
@@ -9,48 +9,22 @@ import uvicorn
 
 app = FastAPI()
 app.include_router(router=router)
-#test teeest
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1",
-    "http://forprojectstests.ru",
-    "http://forprojectstests.ru",
-    "http://fronttest.forprojectstests.ru"
-    "http://fronttest1.forprojectstests.ru"
-    "http://fronttest2.forprojectstests.ru"
-    "https://fronttest3.forprojectstests.ru"
-    # "http://api.forprojectstests.ru",
-
-    "https://localhost",
-    "https://localhost:8000",
-    "https://127.0.0.1:8000",
-    "https://127.0.0.1:5173",
-    "http://localhost:5173",
-    "https://127.0.0.1",
-    "https://forprojectstests.ru",
-    "https://fronttest.forprojectstests.ru"
-    "https://fronttest1.forprojectstests.ru"
-    "https://fronttest2.forprojectstests.ru"
-    "https://fronttest3.forprojectstests.ru"
-    # "https://api.forprojectstests.ru",
-]
-    #
+# test teeest
+origins = ["https://forprojectstests.ru", "https://fronttest.forprojectstests.ru"]
+#
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
-)   
+)
 #
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=False, host="0.0.0.0", port=8000) 
-    # uvicorn.run("main:app", reload=False,  port=8000, ssl_keyfile="key.pem", ssl_certfile="cert.pem") 
-    # uvicorn.run("main:app", reload=False) 
- 
-'''uvicorn main:app --reload'''
+    uvicorn.run("main:app", reload=False, host="0.0.0.0", port=8000)
+    # uvicorn.run("main:app", reload=False,  port=8000, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
+    # uvicorn.run("main:app", reload=False)
+
+"""uvicorn main:app --reload"""
 
 # 1) исправить баги если они будут (они будут наверное =( )

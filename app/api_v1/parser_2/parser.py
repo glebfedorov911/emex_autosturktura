2ca20cb49ecf42c4e14db0374391c32489a75a5f
gfedorov911 <fedorov22134@gmail.com>
2024-09-07 11:56:48 +0300

    edit timeout

--- app/api_v1/parser_2/parser.py
+++ app/api_v1/parser_2/parser.py
@@ -53,9 +53,9 @@ async def main(brands, user_id):
                 page = await browser.new_page()
 
                 try:
-                    await page.goto(url, timeout=2500)
+                    await page.goto(url, timeout=4444)
                 except:
-                    await page.goto(url, timeout=2500)
+                    await page.goto(url, timeout=4444)
 
                 pre = await (await page.query_selector("pre")).text_content()
                 response = dict(json.loads(pre))
@@ -119,9 +119,9 @@ async def main(brands, user_id):
                     best_data = sorted_by_price[0]
 
                     try:
-                        await page.goto(f"https://emex.ru/api/search/rating?offerKey={best_data[0]}", timeout=2500)
+                        await page.goto(f"https://emex.ru/api/search/rating?offerKey={best_data[0]}", timeout=4444)
                     except:
-                        await page.goto(f"https://emex.ru/api/search/rating?offerKey={best_data[0]}", timeout=2500)
+                        await page.goto(f"https://emex.ru/api/search/rating?offerKey={best_data[0]}", timeout=4444)
 
                     pre_with_logo = await (await page.query_selector("pre")).text_content()
                     response_with_logo = dict(json.loads(pre_with_logo))
@@ -135,7 +135,7 @@ async def main(brands, user_id):
                         sorted_by_price = quick_sort(originals, 2)[:20]
                         for data in sorted_by_price:
                             try:
-                                await page.goto(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=2500)
+                                await page.goto(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=4444)
                             except:
                                 sorted_by_price.append(data)
                                 continue

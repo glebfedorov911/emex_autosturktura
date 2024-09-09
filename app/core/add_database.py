edf09d93dc4a2047254d3cefe5992bdd79ea5282
gfedorov911 <fedorov22134@gmail.com>
2024-09-09 20:45:20 +0300

    add print

--- app/core/add_database.py
+++ app/core/add_database.py
@@ -34,11 +34,10 @@ def add_to_db(table, data_add):
         cursor.execute(check_query)
         count = cursor.fetchone()[0]
 
+    print(table)
     if table != "proxys":
         if count != 0:
             return
-    else:
-        print("PFDPDFSPFDPSFDPPD")
         
 
     for values in data_add:

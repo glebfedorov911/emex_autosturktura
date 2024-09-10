6b2af1ccf4e6b9a81f363067bbec28d1f00c9025
gfedorov911 <fedorov22134@gmail.com>
2024-09-10 14:17:02 +0300

    add

--- app/core/models/db_helper.py
+++ app/core/models/db_helper.py
@@ -9,7 +9,11 @@ class DataBaseHelper:
     def __init__(self, url: str, echo: bool = False):
         self.engine = create_async_engine(
             url=url,
-            echo=echo
+            echo=echo,
+            pool_size=5,  
+            max_overflow=10,
+            pool_timeout=30,  
+            pool_recycle=1800
         )
 
         self.session_factory = async_sessionmaker(

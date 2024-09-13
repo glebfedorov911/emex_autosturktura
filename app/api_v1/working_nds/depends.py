1bc803b7a4a813ba2b79fdd4ddc532d3f81175c3
gfedorov911 <fedorov22134@gmail.com>
2024-09-13 16:01:40 +0300

    edit count of threads, add database, depends for saving format

--- app/api_v1/working_nds/depends.py
+++ app/api_v1/working_nds/depends.py
@@ -87,7 +87,7 @@ async def edit_file(filepath: str):
 
     integer_style = NamedStyle(name="integer_style", number_format='0')
 
-    for row in ["K", "M"]:
+    for row in ["J", "K", "L", "M", "N"]:
         for cell in ws[row]:  
             if isinstance(cell.value, (int, float)):
                 cell.style = integer_style

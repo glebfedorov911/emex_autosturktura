8c6c8b3c933b82da4c7f6e9a890474e664d03f8d
gfedorov911 <fedorov22134@gmail.com>
2024-09-13 15:53:25 +0300

    edit count threads, add new proxy, edit format for saving nds

--- app/api_v1/working_nds/depends.py
+++ app/api_v1/working_nds/depends.py
@@ -5,6 +5,10 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.engine import Result
 from sqlalchemy import select
 
+from openpyxl import load_workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import NamedStyle
+
 from app.core.models import Parser, File
 from app.core.config import settings
 
@@ -77,6 +81,18 @@ async def set_filename(file_id: int, session: AsyncSession, user_id: int):
     session.add(file)
     await session.commit()
 
+async def edit_file(filepath: str):
+    wb = load_workbook(filepath)
+    ws = wb.active
+
+    integer_style = NamedStyle(name="integer_style", number_format='0')
+
+    for row in ["K", "M"]:
+        for cell in ws[row]:  
+            if isinstance(cell.value, (int, float)):
+                cell.style = integer_style
+    
+    wb.save(filepath)
 
 async def to_file(filename: str, parser_data: list):
     try:
@@ -132,6 +148,7 @@ async def to_file(filename: str, parser_data: list):
         # Сохранение в файл Excel
         output_path = f"{str(settings.upload.path_for_upload)}/обработанный_{filename}"
         df.to_excel(output_path, index=False)
+        await edit_file(output_path)
 
     except AttributeError as e:
         print(f"Ошибка атрибутов в данных: {e}")

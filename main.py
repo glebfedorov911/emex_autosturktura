1d5f0f6e89cc255d4ba3362ae85201c7c4636b35
gfedorov911 <fedorov22134@gmail.com>
2024-09-25 23:40:33 +0300

    comment two origins

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app.include_router(router=router)
 # test teeest
 origins = [
     "https://autostructure.ru",
-    'http://127.0.0.1:5173',
-    'http://localhost:5173',
+#     'http://127.0.0.1:5173',
+#     'http://localhost:5173',
 ]
 #
 app.add_middleware(

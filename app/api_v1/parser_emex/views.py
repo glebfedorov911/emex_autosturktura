298a01d771b29f197fe8b85cbc9e9533ffbd08cc
gfedorov911 <fedorov22134@gmail.com>
2024-09-27 16:36:49 +0300

    add proxies, edit count threadings

--- app/api_v1/parser_emex/views.py
+++ app/api_v1/parser_emex/views.py
@@ -35,7 +35,7 @@ import pandas as pd
 router = APIRouter(prefix="/new_parser", tags=["New Parser"])
 templates = Jinja2Templates(directory=settings.templates.templates_path)
 
-count_of_threadings = 14
+count_of_threadings = 12
 threads: list[Thread] = [None] * count_of_threadings
 
 

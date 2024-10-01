b9e287cb8b0d9d00f61080972ac4a3bb83645767
gfedorov911 <fedorov22134@gmail.com>
2024-10-01 16:21:54 +0300

    count_of_threadings = 16 (very good, better 32)

--- app/api_v1/parser_emex/views.py
+++ app/api_v1/parser_emex/views.py
@@ -37,7 +37,7 @@ import pandas as pd
 router = APIRouter(prefix="/new_parser", tags=["New Parser"])
 templates = Jinja2Templates(directory=settings.templates.templates_path)
 
-count_of_threadings = 256
+count_of_threadings = 16
 threads: list[Thread] = [None] * count_of_threadings
 
 

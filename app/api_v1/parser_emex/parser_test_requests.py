2c801632ed26bd72f199e30cd1313f4d07879b69
Gleb <fedorov22134@gmail.com>
2024-11-09 00:14:56 +0300

    back

--- app/api_v1/parser_emex/parser_test_requests.py
+++ app/api_v1/parser_emex/parser_test_requests.py
@@ -68,17 +68,7 @@ async def main(user_id, using_proxy):
 
     while user_data[user_id]["status"] == "PARSER_RUNNING":
         headers = {
-            'User-Agent': random.choice(USERAGENTS),
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9",
-            "Connection": "keep-alive",
-            "Referer": "https://emex.ru/",  
-            "Upgrade-Insecure-Requests": "1",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-User": "?1",
-            "Sec-Fetch-Dest": "document",
+            'User-Agent': random.choice(USERAGENTS)
         }
 
         for stop in user_data[user_id]["stop"]:

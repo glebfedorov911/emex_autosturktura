ffeec12c111c78c2943c8fc8f344502f13546af5
Gleb <fedorov22134@gmail.com>
2024-11-09 00:11:43 +0300

    add headers

--- app/api_v1/parser_emex/parser_test_requests.py
+++ app/api_v1/parser_emex/parser_test_requests.py
@@ -67,7 +67,19 @@ async def main(user_id, using_proxy):
         user_data[user_id]["columns"].append("Цена с лого")
 
     while user_data[user_id]["status"] == "PARSER_RUNNING":
-        headers = {'User-Agent': random.choice(USERAGENTS)}
+        headers = {
+            'User-Agent': random.choice(USERAGENTS),
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Referer": "https://emex.ru/",  
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-User": "?1",
+            "Sec-Fetch-Dest": "document",
+        }
 
         for stop in user_data[user_id]["stop"]:
             if stop:
@@ -411,11 +423,11 @@ async def main(user_id, using_proxy):
                     for data in sorted_by_price:
                         try:
                             async with aiohttp.ClientSession() as session:
-                                async with session.get(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=timeout2, http_version="2.0", proxy=proxies, headers=headers) as resp:
+                                async with session.get(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=timeout2, proxy=proxies, headers=headers) as resp:
                                     response_with_logo = await resp.json()
                         except:
                             async with aiohttp.ClientSession() as session:
-                                async with session.get(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=timeout2, http_version="2.0", proxy=proxies, headers=headers) as resp:
+                                async with session.get(f"https://emex.ru/api/search/rating?offerKey={data[0]}", timeout=timeout2, proxy=proxies, headers=headers) as resp:
                                     response_with_logo = await resp.json()
                         price_logo = response_with_logo["priceLogo"]
 

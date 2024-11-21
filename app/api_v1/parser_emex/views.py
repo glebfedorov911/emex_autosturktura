9bfe6f70ba34e98be4205300dcc0d5f2b613e9f0
Gleb <fedorov22134@gmail.com>
2024-11-21 20:06:25 +0300

    change

--- app/api_v1/parser_emex/views.py
+++ app/api_v1/parser_emex/views.py
@@ -38,7 +38,7 @@ import threading
 router = APIRouter(prefix="/new_parser", tags=["New Parser"])
 templates = Jinja2Templates(directory=settings.templates.templates_path)
 
-count_of_threadings = 8
+count_of_threadings = 16
 threads: list[Thread] = [None] * count_of_threadings
 
 

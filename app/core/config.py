ef9854d6c6ecd813e62676dbfc2cd0f9706c9b60
Gleb <fedorov22134@gmail.com>
2025-06-12 19:43:40 +0300

    add /docs

--- app/core/config.py
+++ app/core/config.py
@@ -36,7 +36,7 @@ class UploadFiles(BaseModel):
     path_for_upload: Path = BASE_DIR / "upload_file"
 
 class Docs(BaseModel):
-    docs_url: str = ""
+    docs_url: str = "/docs"
 
 class Settings(BaseSettings):
     db: DBSettings = DBSettings()
